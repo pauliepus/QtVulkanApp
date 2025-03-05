40f61b8fcbf822e96ab58053955d8ce0a602c05b
pauliepus <142883643+pauliepus@users.noreply.github.com>
2025-03-05 16:59:29 +0100

    FINALLY A FUCKING CUBE
    FINALLY CUBENIN

--- TriangleSurface.cpp
+++ TriangleSurface.cpp
@@ -1,23 +1,88 @@
 #include "TriangleSurface.h"
 #include <fstream>
 #include <QDebug>
+
 TriangleSurface::TriangleSurface() : VisualObject()
 {
-    Vertex v1{0.0f,   0.0f,  0.0f,   1.0f, 0.0f, 0.0f, 0.0f, 0.0f}; //red point at origo
-    Vertex v2{1.0f,   0.0f,  0.0f,   0.0f, 1.0f, 0.0f, 0.0f, 0.0f}; //green, point x+1
-    Vertex v3{0.0f,   1.0f,  0.0f,   0.0f, 0.0f, 1.0f, 0.0f, 0.0f}; //blue, point y+1
-    Vertex v4{1.0f,   1.0f,  0.0f,   1.0f, 1.0f, 0.0f, 0.0f, 0.0f}; //x+1,y+1,0, r+g
+    //red front-low-left, point at origo
+    Vertex v1{0.0f,   0.0f,  0.0f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f};
+    //green front-low-right green, point x+1
+    Vertex v2{1.0f,   0.0f,  0.0f,   0.0f, 1.0f, 0.0f,   0.0f, 0.0f};
+    //blue front-top-right, point y+1
+    Vertex v3{0.0f,   1.0f,  0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f};
+    //r+g front-    x+1,y+1,0,
+    Vertex v4{1.0f,   1.0f,  0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 0.0f};
+    //blue bak v3 -low-right x+1,z-1
+    Vertex v5{0.0f,  1.0f, -1.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f};
+    //red bak-v1
+    Vertex v6{0.0f,  0.0f, -1.0f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f};
+    //green bak-v2
+    Vertex v7{1.0f,  0.0f, -1.0f,   0.0f, 1.0f, 0.0f,   0.0f, 0.0f};
+    //r+g bak v4
+    Vertex v8{1.0f,  1.0f, -1.0f,   1.0f, 1.0f, 0.0f,   0.0f, 0.0f};
 
 
 
     //Pushing 1st triangle,
+    mVertices.push_back(v1); //front-low-left
+    mVertices.push_back(v2); //front-low-right
+    mVertices.push_back(v3); //front-top-right
+    //then the 2nd.
+    mVertices.push_back(v3); //front-top-right
+    mVertices.push_back(v2); //front-low-right
+    mVertices.push_back(v4); //front-top-left
+
+// square behind first
+    mVertices.push_back(v6); //bak v1
+    mVertices.push_back(v7);
+    mVertices.push_back(v5);
+
+    mVertices.push_back(v5); //bak 3
+    mVertices.push_back(v7); //bak 2
+    mVertices.push_back(v8); //bak 4
+
+
+// square on top
+
+    mVertices.push_back(v5);
+    mVertices.push_back(v3);
+    mVertices.push_back(v8);
+
+    mVertices.push_back(v4);
+    mVertices.push_back(v8);
+    mVertices.push_back(v3);
+
+
+// square on something
+
+    mVertices.push_back(v5); // blå bak
+    mVertices.push_back(v1); // rød
+    mVertices.push_back(v6); // blå bak
+
+    mVertices.push_back(v3);
+    mVertices.push_back(v5);
+    mVertices.push_back(v1);
+
+
+//right side
+
+    mVertices.push_back(v8);
+    mVertices.push_back(v4);
+    mVertices.push_back(v2);
+
+    mVertices.push_back(v8);
+    mVertices.push_back(v7);
+    mVertices.push_back(v2);
+
+// underside
     mVertices.push_back(v1);
     mVertices.push_back(v2);
-    mVertices.push_back(v3);
-    //then the 2nd.
-    mVertices.push_back(v3);
+    mVertices.push_back(v6);
+
+    mVertices.push_back(v7);
     mVertices.push_back(v2);
-    mVertices.push_back(v4);
+    mVertices.push_back(v6);
+
 
     //Temporary scale and positioning
     mMatrix.scale(0.5f);

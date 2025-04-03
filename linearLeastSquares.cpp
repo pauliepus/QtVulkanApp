5a22658536a355f412497322154a5d09fb929d48
pauliepus <142883643+pauliepus@users.noreply.github.com>
2025-04-03 15:48:16 +0200

    Added parser + PointFile + cmake
    ++

--- linearLeastSquares.cpp
+++ linearLeastSquares.cpp
@@ -15,11 +15,11 @@ void linearLeastSquares(const std::vector<Point>& points, double& Beta0, double&
         Sy += p.y;
         Sxx += p.x * p.x;
         Sxy += p.x * p.y;
-    }    
-
+    }
 
     Beta1 = ((n * Sxy) - (Sx * Sy)) / ((n * Sxx) - (Sx * Sx));
     Beta0 = ((Sy - Beta1 * Sx) / n);
+
 }
 int main() {
     std::vector<Point> points = {{1, 2}, {2, 2.8}, {3, 3.6}, {4, 4.5}, {5, 5.1}};
@@ -29,4 +29,72 @@ int main() {
 
     std::cout << "Best fit line: y = " << a << "x + " << b << std::endl;
     return 0;
+}
+
+void ReadFromFile(const std::string& filename)
+{
+    std::vector<std::vector<std::string>> sPoints;  //vector of vectors of points as strings
+    std::vector<Point> points;                        // vector of points
+    std::vector<std::string> tempVector;            // temporary vector to store single strings of points
+    std::string tempString;
+    std::string::size_type SZ;
+
+    double tempX, tempY;
+    Point tempPoint;
+
+    std::ifstream file(filename);
+
+    if (!file.is_open())
+    {
+        std::cerr << "Failed to open file: " << filename << std::endl;
+        return;
+    }
+
+    std::string line;
+    while (std::getline(file, line))
+    {
+        std::vector<std::string> row;
+        std::stringstream ss(line);
+        std::string cell;
+
+        while (std::getline(ss, cell, ','))
+        {
+            row.push_back(cell);
+        }
+
+        sPoints.push_back(row);
+    }
+
+    file.close();
+
+    for (int i = 0; i < (((sizeof(sPoints) - 1) * 2) - 1); i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            if (j < 1) // when we get to the X point
+            {
+                tempString = sPoints[i][j] + " " + sPoints[i][j + 1]; // adds both points as one string
+                tempVector.push_back(tempString);
+            }
+
+        }
+    }
+
+    for (int i = 1; i < (((sizeof(tempVector) - 1) * 2) - 1); i++)
+    {
+        tempX = std::stod(tempVector[i], &SZ);
+        tempY = std::stod(tempVector[i].substr(SZ));
+        tempPoint = { tempX, tempY };
+        points.push_back(tempPoint);
+    }
+
+
+    for (int i = 0; i < points.size(); i++)
+    {
+        std::cout << points[i].x << ", " << points[i].y << std::endl;
+    }
+    std::cout << points.size() << std::endl;
+    double a, b;
+    linearLeastSquares(points, a, b);
+    std::cout << "Best fit line: y = " << a << "x + " << b << std::endl;
 }

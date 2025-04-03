6bdca1759808d60919f65fd1f4f7cc352a1a304b
pauliepus <142883643+pauliepus@users.noreply.github.com>
2025-04-03 15:19:40 +0200

    Fixed LinearLeastSquares
    Probly :)

--- linearLeastSquares.cpp
+++ linearLeastSquares.cpp
@@ -10,17 +10,16 @@ void linearLeastSquares(const std::vector<Point>& points, double& Beta0, double&
     int n = points.size();
     double Sx = 0, Sy = 0, Sxx = 0, Sxy = 0; //sxy is sum of x*y
 
-    //sx is sum of ALL x values, Sy, is sum of all Y values, Sxx should be
-
     for (const auto& p : points) {
         Sx += p.x;
+        Sy += p.y;
+        Sxx += p.x * p.x;
+        Sxy += p.x * p.y;
+    }    
 
-        // Compute all necessary sum
 
-    }
-
-    // Beta1 = // complete the code
-    // Beta0 = // complete the code
+    Beta1 = ((n * Sxy) - (Sx * Sy)) / ((n * Sxx) - (Sx * Sx));
+    Beta0 = ((Sy - Beta1 * Sx) / n);
 }
 int main() {
     std::vector<Point> points = {{1, 2}, {2, 2.8}, {3, 3.6}, {4, 4.5}, {5, 5.1}};

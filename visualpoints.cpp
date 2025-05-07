69edbb7669fa4f9686b949f4606beab8e69baf58
pauliepus <142883643+pauliepus@users.noreply.github.com>
2025-05-07 10:08:47 +0200

    OK it's fixed??
    Now to head on to show it visually

---
+++ visualpoints.cpp
@@ -0,0 +1,3 @@
+#include "visualpoints.h"
+
+visualpoints::visualpoints() {}
---
+++ visualpoints.h
@@ -0,0 +1,12 @@
+#ifndef VISUALPOINTS_H
+#define VISUALPOINTS_H
+
+#include "VisualObject.h"
+
+class visualpoints : public VisualObject
+{
+public:
+    visualpoints();
+};
+
+#endif // VISUALPOINTS_H
